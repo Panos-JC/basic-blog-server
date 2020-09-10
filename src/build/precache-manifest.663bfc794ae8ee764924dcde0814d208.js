9dd3844bbf857bf30bdc7a16e75dae46d5628a23
Panos-JC <phatziioannou@gmail.com>
2020-09-10 14:14:18 +0300

    First commit

---
+++ src/build/precache-manifest.663bfc794ae8ee764924dcde0814d208.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c169845e042fd198ac432cc90e547053",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97909aebaee359727058",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "be061a2a891ee5256214",
+    "url": "/static/js/2.db04eed3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.db04eed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97909aebaee359727058",
+    "url": "/static/js/main.f0adc971.chunk.js"
+  },
+  {
+    "revision": "852640fd33654f1428f6",
+    "url": "/static/js/runtime-main.9406f14f.js"
+  }
+]);
